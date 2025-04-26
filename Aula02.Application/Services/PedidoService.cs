779ebaf2f4cc63ee807395c661804818292f192c
Heitor Hidalgo <heitorhidalgo10@hotmail.com>
2025-04-26 12:12:08 -0300

    aula 26/04 não finalizada

---
+++ Aula02.Application/Services/PedidoService.cs
@@ -0,0 +1,83 @@
+﻿using Aula02.Domain.Interfaces;
+using Aula02.Domain.Models;
+
+namespace Aula02.Application.Services
+{
+    public class PedidoService : IPedidoService
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        public PedidoService(IPedidoRepository pedidoRepository) 
+        { 
+            _pedidoRepository = pedidoRepository;
+        }
+
+        public async Task<bool> AtualizarPedido(Pedido pPedido)
+        {
+            try
+            {
+                Pedido pedido = await _pedidoRepository.RetornaPedidoPorIdAsync(pPedido.Id);
+
+                if (pedido == null)
+                    return false;
+
+                pedido.DataPedido = pPedido.DataPedido;
+                pedido.ValorTotal = pPedido.ValorTotal;
+                pedido.ClienteId = pPedido.ClienteId;
+
+                await _pedidoRepository.AtualizarPedidoAsync(pPedido);
+            }
+            catch (Exception ex) 
+            {
+                throw;
+            }
+
+            return true;
+        }
+
+        public async Task<Pedido> CriarPedido(Pedido pPedido)
+        {
+            try
+            {
+                await _pedidoRepository.CriarPedidoAsync(pPedido);
+
+                return pPedido;
+            }
+            catch (Exception ex) 
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> ExcluirPedido(Pedido pPedido)
+        {
+            try
+            {
+                Pedido pedido = await _pedidoRepository.RetornaPedidoPorIdAsync(pPedido.Id);
+
+                if (pedido == null)
+                    return false;
+
+                await _pedidoRepository.ExcluirPedidoAsync(pedido);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public async Task<IEnumerable<Pedido>> RetornaTodosPedidosAsync()
+        {
+            try
+            {
+                IEnumerable<Pedido> pedidos = await _pedidoRepository.RetornaTodosPedidosAsync();
+
+                return pedidos;
+            }
+            catch (Exception ex) 
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Aula02.Domain/Interfaces/IPedidoService.cs
@@ -0,0 +1,17 @@
+﻿using Aula02.Domain.Models;
+
+namespace Aula02.Domain.Interfaces
+{
+    public interface IPedidoService
+    {
+        Task<Pedido> RetornaPedidoPorId(int pId);
+        Task<IEnumerable<Pedido>> RetornaTodosPedidosAsync();
+
+        Task<Pedido> CriarPedidoAsync(Pedido pPedido);
+
+        Task<bool> AtualizarPedidoAsync(Pedido pPedido);
+
+        Task<bool> ExcluirPedidoAsync(int pId);
+
+    }
+}
